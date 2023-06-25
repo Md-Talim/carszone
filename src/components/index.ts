d0813c3706566268faf8ec73649d87d62e82152c
Mohammad Talim <talimmohammad116@gmail.com>
2023-06-25 11:37:08 +0530

    Build searchBar and implement search functionality

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,7 @@ import Hero from './Hero';
 import Button from './Button';
 import Footer from './Footer';
 import Navbar from './Navbar';
+import SearchBar from './SearchBar';
+import SearchManufacturer from './SearchManufacturer';
 
-export { Hero, Button, Footer, Navbar };
+export { Hero, Button, Footer, Navbar, SearchBar, SearchManufacturer };
